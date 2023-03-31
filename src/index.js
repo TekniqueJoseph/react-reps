4459ddbb03a251cd94d8023063cb11103acecd06
TekniqueJoseph <joejacksun@gmail.com>
2023-03-31 19:02:07 -0400

    updated note

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Hello My Boy!</h1>
     <App />
   </React.StrictMode>
 );
